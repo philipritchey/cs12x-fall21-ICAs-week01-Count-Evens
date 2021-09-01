731a644c9492418b17d601b0a5f9afe2ed92d76c
Philip Ritchey <philipritchey@users.noreply.github.com>
2021-09-01 18:41:19 +0000

    added solution code.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ output: print to standard output the number of even numbers in the input sequenc
 // never "using namespace std;"
 using std::cin, std::cout, std::endl;
 
+void count_evens();
+
 int main() {
     // recipe for reading 0-terminated sequence of integers from standard input
     //     warning: integer overflow is a hazard
@@ -19,4 +21,18 @@ int main() {
     }
 
     // TODO(student): modify/augment the code above to count the even numbers.
+}
+
+// ritchey's solution
+void count_evens() {
+    int evens_cnt = 0;
+    int number;
+    cin >> number;
+    while (number != 0) {
+        if (number % 2 == 0) {
+            evens_cnt++;
+        }
+        cin >> number;
+    }
+    cout << evens_cnt << endl;
 }
